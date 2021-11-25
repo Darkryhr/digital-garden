d9e67eca19430f5101ee99c2fff3f50a40101670
Darkryhr <gabyk800@gmail.com>
2021-11-25 15:11:50 +0200

    switched to unified for markdown, +markdown styling

--- components/post-body.js
+++ components/post-body.js
@@ -12,7 +12,37 @@ const PostBody = ({ content }) => {
 export default PostBody;
 
 const MarkdownStyles = styled.div`
-  & > p {
+  p {
+    color: ${({ theme }) => theme.colors.text};
+    font-size: 16px;
+  }
+  h2 {
+    font-weight: 600;
+    font-size: 20px;
     padding: 1em 0;
+  }
+  h1 {
+    font-weight: 700;
+    font-size: 24px;
+    padding-bottom: 5px;
+  }
+  hr {
+    opacity: 0.2;
+  }
+  strong {
+    font-weight: 600 !important;
+    display: block;
+    color: ${({ theme }) => theme.colors.accent};
+    font-size: 18px;
+    padding: 0.4em 0;
+  }
+  ul {
+    list-style: circle;
+  }
+  li {
+    padding-bottom: 3px;
+  }
+  em {
+    font-weight: 600;
   }
 `;
--- components/styled/globalStyles.js
+++ components/styled/globalStyles.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyles = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.secondary};
     transition: all 0.50s linear;
   }
   a {
