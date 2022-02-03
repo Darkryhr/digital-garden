cc70a4bee46358c2bdc20bbedfcd84819a310f9b
Darkryhr <gabyk800@gmail.com>
2022-02-03 08:45:01 +0200

    scrollbar style

--- components/styled/globalStyles.js
+++ components/styled/globalStyles.js
@@ -1,20 +1,38 @@
 import { createGlobalStyle } from 'styled-components';
 export const GlobalStyles = createGlobalStyle`
-*{
+  * {
   color: ${({ theme }) => theme.colors.text};
+  }
 
-}
   body {
     color: ${({ theme }) => theme.colors.text};
     transition: all 0.50s linear;
-background:${({ theme }) => theme.colors.primary};
+    background:${({ theme }) => theme.colors.primary};
   }
-  a {
-  text-decoration: none;
-  color: ${({ theme }) => theme.colors.text};
-  }
-  button {
-  color: ${({ theme }) => theme.colors.text};
 
+  a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.text};
   }
-  `;
+
+  button {
+    color: ${({ theme }) => theme.colors.text};
+  }
+
+  ::-webkit-scrollbar {
+    width:7px;
+  }
+  
+  ::-webkit-scrollbar-track {
+    background:${({ theme }) => theme.colors.primary};
+    border-radius: 10px;
+  }
+  
+  ::-webkit-scrollbar-thumb {
+    background:${({ theme }) => theme.colors.secondary};
+    border-radius: 10px;
+    &:hover {
+      background:${({ theme }) => theme.colors.accent};
+    }
+  }
+`;
