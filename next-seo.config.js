db0c3037551da42efbd0898877cb12a62460556f
Gabriel Lellouche <gabyk800@gmail.com>
2022-07-28 19:55:19 +0300

    updated next-seo config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,13 +1,20 @@
 export const SEOConfig = {
+  title: 'Gabriel does a thing',
+  description: 'Personal digital garden',
   openGraph: {
     type: 'website',
     locale: 'en_IE',
-    url: 'https://www.url.ie/',
-    site_name: 'SiteName',
+    url: 'https://www.gabriel-lellouche.com/',
+    title: 'Gabriel does a thing',
+    description: 'Personal digital garden',
+    image: 'https://www.gabriel-lellouche.com/card.png',
+    site_name: 'GabrielLellouche.com',
+    imageWidth: 400,
+    imageHeight: 200,
   },
   twitter: {
-    handle: '@handle',
-    site: '@site',
+    handle: '@GabrielAintReal',
+    site: 'https://www.gabriel-lellouche.com/',
     cardType: 'summary_large_image',
   },
 };
