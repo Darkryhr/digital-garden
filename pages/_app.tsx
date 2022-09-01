9fa465f305dc78582c7c5c01df0edb910905c6b5
Gabriel Lellouche <gabyk800@gmail.com>
2022-09-01 18:49:24 +0300

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import type { AppProps } from 'next/app';
+import { MDXProvider } from '@mdx-js/react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { ThemeProvider } from 'styled-components';
-import { Toaster } from 'react-hot-toast';
-import { SEOConfig } from '../next-seo.config';
+import { DefaultSeo } from 'next-seo';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import 'normalize.css';
+import React, { useEffect, useState } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { ThemeProvider } from 'styled-components';
+import { SEOConfig } from '../next-seo.config';
 
-import { useDarkMode } from 'hooks/useDarkMode';
-import { lightTheme, darkTheme } from '@styled/theme.css';
-import { GlobalStyles } from '@styled/global.css';
 import Layout from '@components/Layout';
 import Loader from '@components/Loader';
-import { MDXProvider } from '@mdx-js/react';
 import MDXComponents from '@components/mdx';
-import { DefaultSeo } from 'next-seo';
+import * as gtag from '@lib/gtag';
+import { GlobalStyles } from '@styled/global.css';
+import { darkTheme, lightTheme } from '@styled/theme.css';
+import { useDarkMode } from 'hooks/useDarkMode';
 
 function App({ Component, pageProps, router }: AppProps) {
   const [theme, themeToggler, mountedComponent] = useDarkMode();
   const themeMode = theme === 'light' ? lightTheme : darkTheme;
   const [pageLoading, setPageLoading] = useState(false);
+
+  //* for page loading
   useEffect(() => {
     const handleStart = () => {
       setPageLoading(true);
@@ -30,12 +34,43 @@ function App({ Component, pageProps, router }: AppProps) {
     router.events.on('routeChangeStart', handleStart);
     router.events.on('routeChangeComplete', handleComplete);
     router.events.on('routeChangeError', handleComplete);
-  }, [router]);
+  }, [router.events]);
+
+  //* for analytics
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on('hashChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('hashChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   if (!mountedComponent) return <div />;
 
   return (
     <ThemeProvider theme={themeMode}>
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <MDXProvider components={MDXComponents}>
         <GlobalStyles />
         <Layout toggleTheme={themeToggler}>
